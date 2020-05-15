08c8a93238b9958d7a3a93d1d6caff57a9220ac9
ArifSanaullah <ak7896023@gmail.com>
2020-05-15 18:02:11 +0500

    feat: Add unknown(404) route handler

--- 13MayClass/server.js
+++ 13MayClass/server.js
@@ -17,8 +17,22 @@ app.post("/signin/", middlewares.authMiddleware, apis.signInApi);
 app.post("/createuser", middlewares.createUserMiddleware, apis.createUserApi);
 app.post("/getuser", middlewares.getUserMiddleware, apis.getUserApi);
 app.put("/update-user", middlewares.updateUserMiddleware, apis.updateUser);
-app.delete("/delete-user", middlewares.deleteUserMiddleware, apis.deleteUserApi);
+app.delete(
+  "/delete-user",
+  middlewares.deleteUserMiddleware,
+  apis.deleteUserApi
+);
 app.use("/albums", albums);
+
+app.use((req, res, next) => {
+  req.status = 404;
+  const error = new Error("Page not found");
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(req.status).send({ error: { message: error.message } });
+});
 
 module.exports = ({ port, cb }) => {
   app.listen(port, cb);
