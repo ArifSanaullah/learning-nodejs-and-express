c827f2794336b757eafc24a4ab74552ca57a078d
ArifSanaullah <ak7896023@gmail.com>
2020-05-17 00:11:57 +0500

    feat: Add Delete/Update to /albums

--- 13MayClass/routers/albums.js
+++ 13MayClass/routers/albums.js
@@ -1,6 +1,8 @@
 const albums = require("express").Router();
+
 const tracks = require("./tracks");
 const Album = require("../models/Album");
+const Track = require("../models/Track");
 
 albums.get("/", async (req, res) => {
   try {
@@ -39,11 +41,56 @@ albums.post("/", async (req, res) => {
 
 albums.use(
   "/:albumId/tracks",
-  (req, res, next) => {
-    req.albumId = req.params.albumId;
-    next();
+  async (req, res, next) => {
+    try {
+      const foundAlbum = await Album.findById(req.params.albumId);
+
+      if (!foundAlbum)
+        return res.status(404).send({ message: "Album not found" });
+
+      req.albumId = req.params.albumId;
+      next();
+    } catch (err) {
+      res.status(500).send({ message: err.message });
+    }
   },
   tracks
 );
+
+albums.put("/:albumId", async (req, res) => {
+  try {
+    const updatedProps = {};
+    for (const prop of req.body) {
+      updatedProps[prop.propName] = prop.value;
+    }
+
+    const prevAlbum = await Album.findByIdAndUpdate(req.params.albumId, {
+      $set: updatedProps,
+    });
+
+    if (!prevAlbum) return res.status(404).send({ message: "Album not found" });
+
+    res.status(200).send(prevAlbum);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+albums.delete("/:albumId", async (req, res) => {
+  try {
+    const { albumId } = req.params;
+    const albumFound = await Album.findById(albumId);
+
+    if (!albumFound)
+      return res.status(404).send({ message: "Album not found" });
+
+    await Album.findByIdAndDelete(albumId);
+    await Track.deleteMany({ albumId });
+
+    res.status(200).send({ message: "Album Deleted successfully!" });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
 
 module.exports = albums;
