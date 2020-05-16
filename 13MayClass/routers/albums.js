899dbc3e0a5b69a3b629afdfa6ac5444dbd7cd4d
ArifSanaullah <ak7896023@gmail.com>
2020-05-16 22:08:26 +0500

    feat: add Update/Delete operations to /tracks route + refactoring

--- 13MayClass/routers/albums.js
+++ 13MayClass/routers/albums.js
@@ -6,27 +6,35 @@ albums.get("/", async (req, res) => {
   try {
     const albums = await Album.find({});
     if (!albums) return res.status(404).send({ message: "No albums found" });
+
     res.status(200).send({ count: albums.length, albums });
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
 });
 
-albums.get("/:albumId", (req, res) => {
-  const myAlbum = req.params.albumId;
-  res.send(myAlbum);
+albums.get("/:albumId", async (req, res) => {
+  const albumId = req.params.albumId;
+  try {
+    const album = await Album.findOne({ _id: albumId });
+    if (!album) return res.status(404).send({ message: "No album found" });
+
+    res.status(200).send(album);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
 });
 
-albums.post("/", (req, res) => {
-  const { name, tracks = [], singer } = req.body;
-  Album({ name, tracks, singer })
-    .save()
-    .then((result) => {
-      res.status(200).send(result);
-    })
-    .catch((err) => {
-      res.status(500).send({ message: err.message });
-    });
+albums.post("/", async (req, res) => {
+  try {
+    const { name, tracks = [], singer } = req.body;
+
+    await new Album({ name, tracks, singer }).save();
+
+    res.status(200).send({ message: "Album created successfully!" });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
 });
 
 albums.use(
--- 13MayClass/routers/tracks.js
+++ 13MayClass/routers/tracks.js
@@ -3,9 +3,27 @@ const Track = require("../models/Track");
 const Album = require("../models/Album");
 
 tracks.get("/", async (req, res) => {
+  if (req.albumId) {
+    const { albumId } = req;
+
+    try {
+      const album = await Album.findOne({ _id: albumId });
+      if (!album) return res.status(404).send({ message: "No tracks found" });
+
+      const tracks = await Track.find({ albumId });
+      if (!tracks) return res.status(404).send({ message: "No tracks found" });
+
+      return res.status(200).send(tracks);
+    } catch (err) {
+      return res.status(500).send({ message: err.message });
+    }
+  }
+
   try {
     const tracks = await Track.find({}).select("name _id albumId");
+
     if (!tracks) return res.status(404).send({ message: "No tracks found" });
+
     res.status(200).send(tracks);
   } catch (err) {
     res.status(500).send({ message: err.message });
@@ -16,27 +34,109 @@ tracks.post("/", async (req, res, next) => {
   const { name, albumId } = req.body;
   try {
     const findAlbum = await Album.findById(albumId);
-
     if (!findAlbum) return res.status(404).send({ message: "Album not found" });
 
     const track = await new Track({ name, albumId }).save();
+
     const newTracks = [
       ...findAlbum.tracks.map((t) => ({ name: t.name, _id: t._id })),
       { name: track.name, _id: track._id },
     ];
 
     await Album.updateOne({ _id: albumId }, { $set: { tracks: newTracks } });
+
     res.status(201).send({ message: "Track created successfully!" });
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
 });
 
-tracks.get("/:trackId", (req, res) => {
+tracks.get("/:trackId", async (req, res) => {
   const trackId = req.params.trackId;
-  const albumId = req.albumId;
 
-  res.send({ trackId, albumId });
+  try {
+    const track = await Track.findOne({ _id: trackId });
+
+    if (!track) return res.status(404).send({ message: "Track not found" });
+
+    res.status(200).send(track);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+tracks.put("/:trackId", async (req, res) => {
+  /**
+   * req.body syntax =>
+   * [
+   *    {"propName": "nameOfTheExistingPropertyThatNeedsToBeUpdated", value: "newValueOfTheProp"},
+   *    {"propName": "nameOfTheExistingPropertyThatNeedsToBeUpdated", value: "newValueOfTheProp"},
+   * ]
+   */
+  try {
+    const trackId = req.params.trackId;
+    const updatedProps = {};
+    for (const prop of req.body) {
+      updatedProps[prop.propName] = prop.value;
+    }
+
+    const prevTrack = await Track.findByIdAndUpdate(trackId, {
+      $set: updatedProps,
+    });
+
+    if (!prevTrack) {
+      return res.status(404).send({
+        message: "Track not found",
+      });
+    }
+
+    const updatedTrack = await Track.findOne(updatedProps);
+    const trackAlbum = await Album.findOne({ _id: updatedTrack.albumId });
+
+    const updatedTrackAlbum = trackAlbum.tracks.map((track) =>
+      track._id.toString() === updatedTrack._id.toString()
+        ? updatedTrack
+        : track
+    );
+
+    await Album.findOneAndUpdate(
+      { _id: updatedTrack.albumId },
+      { $set: { tracks: updatedTrackAlbum } }
+    );
+
+    res.status(200).send({ message: "Track successfully updated!" });
+  } catch (err) {
+    res.status(500).send({
+      message: err.message,
+    });
+  }
+});
+
+tracks.delete("/:trackId", async (req, res) => {
+  try {
+    trackFound = await Track.findById(req.params.trackId);
+    if (!trackFound)
+      return res.status(404).send({ message: "Track not found" });
+
+    const deletedTrack = await Track.findOneAndDelete({
+      _id: req.params.trackId,
+    });
+    const deletedTrackAlbum = await Album.findById(deletedTrack.albumId);
+
+    await Album.findByIdAndUpdate(deletedTrack.albumId, {
+      $set: {
+        tracks: deletedTrackAlbum.tracks.filter(
+          (track) => track._id.toString() !== deletedTrack._id.toString()
+        ),
+      },
+    });
+
+    res.status(200).send({ message: "Track deleted successfully!" });
+  } catch (err) {
+    res.status(500).send({
+      message: err.message,
+    });
+  }
 });
 
 module.exports = tracks;
